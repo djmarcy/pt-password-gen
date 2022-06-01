82eb69e437c7aad69c0e6be95b62474a19c2ec5b
djmarcy <dj_marcy@hotmail.com>
2022-06-01 11:40:34 -0600

    Stops password with too few or too many characters from being generated

--- assets/script.js
+++ assets/script.js
@@ -2,80 +2,76 @@
 var generateBtn = document.querySelector("#generate");
 
 // Password Character Options
-const lowerCase = document.getElementById("lowercase-lttrs")
-const upperCase = document.getElementById("uppercase-lttrs")
-const numbers = document.getElementById("numbers")
-const symbols = document.getElementById("symbols")
-
+const lowerCase = document.getElementById("lowercase-lttrs");
+const upperCase = document.getElementById("uppercase-lttrs");
+const numbers = document.getElementById("numbers");
+const symbols = document.getElementById("symbols");
 
 // Write password to the #password input
 function writePassword() {
-
   // Write password
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-
-
 function generatePassword() {
-  var passwordLength = document.getElementById("pw-length").value
+  var passwordLength = document.getElementById("pw-length").value;
 
   // Fail States
-  if (lowerCase.checked == false &&
+  if (
+    lowerCase.checked == false &&
     upperCase.checked == false &&
     numbers.checked == false &&
-    symbols.checked == false) {
-      alert("Please choose at least one character set.")
+    symbols.checked == false
+  ) {
+    return alert("Please choose at least one character set.");
   }
 
   if (passwordLength < 8 || passwordLength > 128) {
-    alert("Please choose a valid password length.")
+    return alert("Please choose a valid password length.");
   }
 
   // Evaluate which character sets should be included in password
-  let charSet = ""
-  let lowerChars = "abcdefghijklmnopqrstuvwxyz"
-  let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  let nums = "1234567890"
-  let syms = "!@#$%^&*()"
+  let charSet = "";
+  let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let nums = "1234567890";
+  let syms = "!@#$%^&*()";
 
   if (lowerCase.checked) {
-    charSet = charSet += lowerChars
-  } 
+    charSet = charSet += lowerChars;
+  }
 
   if (upperCase.checked) {
-    charSet = charSet += upperChars
+    charSet = charSet += upperChars;
   }
 
   if (numbers.checked) {
-    charSet = charSet += nums
+    charSet = charSet += nums;
   }
 
   if (symbols.checked) {
-    charSet = charSet += syms
+    charSet = charSet += syms;
   }
 
-  console.log(charSet)
-  charSet = charSet.split("")
-  console.log(charSet)
+  console.log(charSet);
+  charSet = charSet.split("");
+  console.log(charSet);
 
   // Choose random password
-  let password = ""
+  let password = "";
   for (i = 0; i < passwordLength; i++) {
-    pwLength = Math.floor(Math.random() * charSet.length)
+    pwLength = Math.floor(Math.random() * charSet.length);
 
-    password = password += charSet[pwLength]
+    password = password += charSet[pwLength];
   }
 
-  toString(password)
+  toString(password);
 
-  console.log(password)
   return password;
-};
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
